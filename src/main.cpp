2fd767ba2c0e8a702994bb1f526857c26264cc16
uguva <itzsenkofox@gmail.com>
2026-06-19 17:19:19 +0700

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -11,10 +11,9 @@ using std::endl;
 using std::string;
 using std::exception;
 
-// Перечисления для навигации главного меню
-enum class MenuOption { Exit = 0, RSA = 1, DiffieHellman = 2, AES = 3, DES = 4, Affine = 5, Vernam = 6 };
+// Добавлены новые алгоритмы
+enum class MenuOption { Exit = 0, RSA = 1, DiffieHellman = 2, AES = 3, DES = 4, Affine = 5, Vernam = 6, Trithemius = 7, Blowfish = 8 };
 
-// Включение Windows.h с защитой от конфликта типов byte
 #ifdef _WIN32
     #define byte _windows_byte
     #include <windows.h>
@@ -23,7 +22,6 @@ enum class MenuOption { Exit = 0, RSA = 1, DiffieHellman = 2, AES = 3, DES = 4, 
 
 int main() {
 #ifdef _WIN32
-    // Настройка локализации UTF-8 для Windows
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 #endif
@@ -35,7 +33,6 @@ int main() {
 
     ModuleManager manager;
     try {
-        // Кроссплатформенная загрузка библиотек плагинов
         #ifdef _WIN32
             manager.load_module("./plugins/rsa/librsa.dll");
             manager.load_module("./plugins/DiffieHellman/libdh.dll");
@@ -43,6 +40,8 @@ int main() {
             manager.load_module("./plugins/des/libdes.dll");
             manager.load_module("./plugins/affine/libaffine.dll");
             manager.load_module("./plugins/vernam/libvernam.dll");
+            manager.load_module("./plugins/trithemius/libtrithemius.dll");
+            manager.load_module("./plugins/blowfish/libblowfish.dll");
         #else
             manager.load_module("./plugins/rsa/librsa.so");
             manager.load_module("./plugins/DiffieHellman/libdh.so");
@@ -50,6 +49,8 @@ int main() {
             manager.load_module("./plugins/des/libdes.so");
             manager.load_module("./plugins/affine/libaffine.so");
             manager.load_module("./plugins/vernam/libvernam.so");
+            manager.load_module("./plugins/trithemius/libtrithemius.so");
+            manager.load_module("./plugins/blowfish/libblowfish.so");
         #endif
     } catch (const exception& e) {
         std::cerr << "\n[ВНИМАНИЕ]: " << e.what() << "\n" << endl;
@@ -57,10 +58,12 @@ int main() {
 
     while (true) {
         cout << "\n=== ГЛАВНОЕ МЕНЮ ===" << endl;
-        cout << "1. Протокол RSA\n2. Протокол Diffie-Hellman\n3. Протокол AES\n4. Протокол DES\n5. Аффинный шифр\n6. Шифр Вернама\n0. Выход из программы" << endl;
+        cout << "1. Протокол RSA\n2. Протокол Diffie-Hellman\n3. Протокол AES\n4. Протокол DES\n"
+             << "5. Аффинный шифр\n6. Шифр Вернама\n7. Шифр Тритемиуса\n8. Протокол Blowfish\n0. Выход" << endl;
         cout << "Выберите операцию: ";
         
-        MenuOption choice = static_cast<MenuOption>(get_choice<int>(0, 6));
+        // Обновите лимит выбора с 6 до 8
+        MenuOption choice = static_cast<MenuOption>(get_choice<int>(0, 8));
 
         switch (choice) {
             case MenuOption::RSA:           run_sub_menu(manager.get_module("RSA"), "RSA"); break;
@@ -69,6 +72,8 @@ int main() {
             case MenuOption::DES:           run_sub_menu(manager.get_module("DES"), "DES"); break;
             case MenuOption::Affine:        run_sub_menu(manager.get_module("Affine"), "Аффинный шифр"); break;
             case MenuOption::Vernam:        run_sub_menu(manager.get_module("Vernam"), "Шифр Вернама"); break;
+            case MenuOption::Trithemius:    run_sub_menu(manager.get_module("Trithemius"), "Шифр Тритемиуса"); break;
+            case MenuOption::Blowfish:      run_sub_menu(manager.get_module("Blowfish"), "Blowfish"); break;
             case MenuOption::Exit:          cout << "Завершение работы..." << endl; return 0;
             default: break;
         }
